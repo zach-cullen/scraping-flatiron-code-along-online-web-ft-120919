44f46251952d9073882fe83e7335c72522347567
Zach Cullen <zacullen@gmail.com>
2020-01-09 21:02:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    p doc.css(".post")
+    puts doc.css(".post")
   end
   
 end
